2cd91711f1f684d1b05c2543b958882cae203ff1
Rahul Ravi <rahulravics171@gmail.com>
2026-05-27 16:04:09 -0700

    Basic layout

---
+++ src/components/CVPreviewContainer.jsx
@@ -0,0 +1,8 @@
+export default function CVPreviewContainer({children}) {
+    return (
+        <div className="cvpreview">
+            <h1> Preview </h1>
+            {children}
+        </div>
+    );
+}
